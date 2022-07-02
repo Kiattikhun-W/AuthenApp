bb779413867b84889241f6d4e030d2028fd18458
Kiattikhun <kiattikhun.wantamard@gmail.com>
2022-07-02 09:46:44 +0700

    add dependency

---
+++ src/page/Login.js
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material"
+
+const Login = (props) => {
+    return (
+        <div>
+            <Container className='bg-red-500'>
+                Login Page
+
+            </Container>
+        </div>)
+}
+export default Login
